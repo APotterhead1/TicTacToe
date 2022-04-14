369ac85784ef35e7b7dbb3f69133dd21f49ac7e8
APotterhead1 <100541647+APotterhead1@users.noreply.github.com>
2022-04-14 07:53:53 -0500

    added constructor to move

--- src/TicTacToe/AI/Move.java
+++ src/TicTacToe/AI/Move.java
@@ -7,8 +7,9 @@ public class Move {
     public Board board;
     public int space;
 
-    public Move() {
+    public Move( Board board, int space ) {
 
-
+        this.board = board;
+        this.space = space;
     }
 }
